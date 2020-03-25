531aaf53494de63fbcb57b99bb924694b96d0d44
David Tanzer <business@davidtanzer.net>
2020-03-25 08:07:20 +0100

    First-load-performance: Styled Components
    Switching to styled components instead of webpack CSS imports improved
    first-load-performance significantly.

--- client/src/MainNavigation.tsx
+++ client/src/MainNavigation.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
 
-import './MainNavigation.css'
+const Nav = styled.ul`
+	display: flex;
+	list-style: none;
+	padding: 0;
+`
+
+const Item = styled.li`
+	margin: 0;
+	padding: 0;
+
+	::after {
+		display: inline;
+		content: "|";
+		padding: 0 0.5em;
+	}
+
+	:last-child::after {
+		content: "";
+	}
+`
 
 export const MainNavigation = () => {
-	return (<ul className="navigation">
-		<li><NavLink to="/">Home</NavLink></li>
-		<li><NavLink to="/r/about">About</NavLink></li>
-		<li><NavLink to="/r/list">List</NavLink></li>
-	</ul>)
+	return (<Nav>
+		<Item><NavLink to="/">Home</NavLink></Item>
+		<Item><NavLink to="/r/about">About</NavLink></Item>
+		<Item><NavLink to="/r/list">List</NavLink></Item>
+	</Nav>)
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,3 @@
-import './index.css'
 import * as serviceWorker from './serviceWorker'
 
 import React from 'react'
