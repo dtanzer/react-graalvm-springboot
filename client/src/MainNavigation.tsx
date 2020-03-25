4513dee3f0135575cca7bb272b74d8bd44a44832
David Tanzer <business@davidtanzer.net>
2020-03-25 07:49:03 +0100

    Added CSS for MainNavigation
    I added some more CSS so that I can test the first-load-performance.

--- client/src/MainNavigation.tsx
+++ client/src/MainNavigation.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+import './MainNavigation.css'
+
 export const MainNavigation = () => {
-	return <ul>
+	return (<ul className="navigation">
 		<li><NavLink to="/">Home</NavLink></li>
 		<li><NavLink to="/r/about">About</NavLink></li>
 		<li><NavLink to="/r/list">List</NavLink></li>
-	</ul>
+	</ul>)
 }
